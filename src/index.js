040ea7e127da77811b9a705474df1c008b9dfde1
Kelvin Vegilla <kelvin.vegilla.715@gmail.com>
2024-01-19 13:36:30 +0800

    :heavy_plus_sign: - Added cors
    used cors to allow cross origin resource sharing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const { server } = require("../config/config");
+const app = express();
 const router = require("./routes");
 
 app.use(express.json());
+app.use(cors());
 app.use("/", router);
 
 app.listen(server.port, () => console.log(`Listening on port ${server.port}`));
